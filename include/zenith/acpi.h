8683622dcb12ad45cec5be4361df33a7562c9c83
Saud Aljuaid <saudaljuaid2013@gmail.com>
2026-08-09 12:36:44 +0300

    Walk ACPI tables and discover MADT

--- include/zenith/acpi.h
+++ include/zenith/acpi.h
@@ -3,6 +3,7 @@
 #define ZENITH_ACPI_H
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
 #include <zenith/boot.h>
@@ -24,7 +25,20 @@ enum acpi_status {
     ACPI_STATUS_BAD_LENGTH,
     ACPI_STATUS_BAD_EXTENDED_CHECKSUM,
     ACPI_STATUS_NULL_ROOT,
-    ACPI_STATUS_ROOT_OUTSIDE_EARLY_MAP
+    ACPI_STATUS_ROOT_OUTSIDE_EARLY_MAP,
+    ACPI_STATUS_BAD_ROOT_SIGNATURE,
+    ACPI_STATUS_BAD_ROOT_LENGTH,
+    ACPI_STATUS_BAD_ROOT_ENTRY_SIZE,
+    ACPI_STATUS_TOO_MANY_ROOT_ENTRIES,
+    ACPI_STATUS_BAD_ROOT_CHECKSUM,
+    ACPI_STATUS_NULL_TABLE,
+    ACPI_STATUS_TABLE_OUTSIDE_EARLY_MAP,
+    ACPI_STATUS_BAD_TABLE_LENGTH,
+    ACPI_STATUS_BAD_TABLE_CHECKSUM,
+    ACPI_STATUS_MISSING_MADT,
+    ACPI_STATUS_DUPLICATE_MADT,
+    ACPI_STATUS_BAD_MADT_LENGTH,
+    ACPI_STATUS_BAD_MADT_FLAGS
 };
 
 struct acpi_root {
@@ -34,11 +48,27 @@ struct acpi_root {
     uint64_t physical_address;
 };
 
+struct acpi_madt {
+    uint64_t physical_address;
+    uint32_t length;
+    uint32_t local_apic_address;
+    uint32_t flags;
+    size_t root_entry_count;
+    uint8_t revision;
+    char oem_id[7];
+    char oem_table_id[9];
+};
+
 enum acpi_status acpi_root_discover(
     const struct boot_context *context,
     struct acpi_root *root
 );
+enum acpi_status acpi_madt_discover(
+    const struct acpi_root *root,
+    struct acpi_madt *madt
+);
 bool acpi_self_test(void);
+bool acpi_tables_self_test(void);
 const char *acpi_root_kind_string(enum acpi_root_kind kind);
 const char *acpi_status_string(enum acpi_status status);
 
